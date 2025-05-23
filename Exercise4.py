e546d5aa7fbe08ebd624323ca57fe137b0ff22cd
asingizwe1 <asingizwe1louis1@gmail.com>
2025-05-23 08:55:40 +0300

    week 1 - exercise 4

---
+++ Exercise4.py
@@ -0,0 +1,19 @@
+age = 20
+message = "I am " + str(age) + " years old."
+print(message)
+
+txt = "      Hello,       Uganda!       "
+final_txt = txt.replace(" ", "")
+print(final_txt)
+
+upper_case = txt.upper()
+print(upper_case)
+
+replaced_txt = txt.replace("U", "V")
+print(replaced_txt)
+
+y = "I am proudly Ugandan"
+print(y[1:4]) 
+
+x = 'All "Data Scientists" are cool!'
+print(x)
