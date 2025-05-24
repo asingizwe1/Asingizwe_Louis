08e9885c0b72d0b5243458858c419146f1fb3cfe
asingizwe1 <asingizwe1louis1@gmail.com>
2025-05-24 06:01:04 +0300

    DAY_3 - implementation 6

---
+++ python_functions.py
@@ -0,0 +1,14 @@
+#functions
+
+#function - reusable block of code
+
+#example 1
+#we use def which means defining
+def add_numbers(a, b):
+    result = a + b
+    print('Sum:',result)
+    return result
+
+add_numbers(5, 10)
+
+
